1ecc37fb67e815ac25cf632f43d4e11b0f73e7e4
cylewis <112433478+cylewis@users.noreply.github.com>
2023-08-02 12:59:35 +0200

    Add files via upload

--- Final/main_menu.gd
+++ Final/main_menu.gd
@@ -1,20 +1,8 @@
 extends CanvasLayer
 
-signal start_game
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func hide_main_menu():
-	$StartButton.hide()
-	$MenuBackground.hide()
-	$Title.hide()
-
 func _on_start_button_pressed():
-	hide_main_menu()
-	start_game.emit()
+	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
